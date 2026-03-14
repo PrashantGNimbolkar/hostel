ab6b32aaf37b890ecb38ca76b3ccb99a28ff44f9
ashwinihiralkar24-design <ashwinihiralkar2425@gmail.com>
2026-03-14 16:45:36 +0530

    add commented code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import React from 'react';
-import './App.css';
-import Login from './Login';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import Login from "./Login";
+
+// hi prashnti
 
 function App() {
   return (
     <div className="App">
       <Login />
-
     </div>
   );
 }
